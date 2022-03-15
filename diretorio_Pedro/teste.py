0e971138d065612c17a2195ce00105589cadae68
Pedro Alexandre <pedro.alexandre_fernandes@hotmail.com>
2022-03-15 11:55:05 -0300

    Criei uma nova função

--- diretorio_Pedro/teste.py
+++ diretorio_Pedro/teste.py
@@ -1,1 +1,14 @@
 print('Vasco da Gama')
+
+def func(qtd_notas):
+    soma_notas = 0
+    i = 1
+    while(i <= qtd_notas):
+        notas = input(f'Insira o valor da nota {i}: ')
+        soma_notas += int(notas)
+        i = i + 1
+
+    return (float)(soma_notas/qtd_notas)
+
+media = func(3);
+print(media)
